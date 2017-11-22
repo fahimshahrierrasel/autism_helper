5f3cc59b947edaf996a60f34d2643270cc72c9f4
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2017-11-22 07:08:18 +0600

    File converted to Java

--- app/src/main/java/com/andromeda/autismhelper/models/Feature.kt
+++
@@ -1,7 +0,0 @@
-package com.andromeda.autismhelper.models
-
-/**
- * Created by fahim on 9/11/17.
- */
-
-public class Feature(var featureName: String?, var featureImage: Int, var activityClass: Class<*>?)
